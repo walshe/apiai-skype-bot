23aa5644dd04ac2a19ee98e29d89e314424d9aa3
Emmett Walsh <emmettwalsh@gmail.com>
2016-07-06 14:36:42 -0400

    custom output

--- skypebot.js
+++ skypebot.js
@@ -3,6 +3,66 @@
 const apiai = require('apiai');
 const uuid = require('node-uuid');
 const skype = require('skype-sdk');
+const _ = require('underscore');
+
+
+
+var db = {
+    restaurant : [
+        {
+            productId: 1,
+            name: "5 Napkin Burger",
+            city: "New York",
+            url: "http://5napkinburger.com/",
+            image: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/5%20Napkin%20Burger/5%20Napkin%20Logo.jpg",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        },
+        {
+            productId: 2,
+            name: "PJ Clarke's",
+            city: "New York",
+            url: "http://pjclarkes.com/",
+            image: "http://www.crainsnewyork.com/apps/pbcsi.dll/storyimage/CN/20100110/SMALLBIZ/301109968/AR/0/P.J.-Clarke's&imageversion=widescreen&maxw=770",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        },
+        {
+            productId: 3,
+            name: "McDonalds",
+            city: "Boston",
+            url: "http://www.mcdonalds.com/us/en/home.html",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mcdonalds-90s-logo.svg/2000px-Mcdonalds-90s-logo.svg.png",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        }
+    ],
+
+    clothing : [
+        {
+            productId: 4,
+            name: "The Gap",
+            city: "New York",
+            url: "http://www.gap.com/",
+            image: "https://lh6.ggpht.com/LKRb7hffPEcZOvKWHUpGo-7ajEYkcMXQw8ewHldpydXfl0hG2K4Ae35NxffRmUU4LZmM=w300",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        },
+        {
+            productId: 5,
+            name: "Banana Republic",
+            city: "New York",
+            url: "http://bananarepublic.com/",
+            image: "http://images.military.com/media/mail/deals-and-discounts/bananarepublic.jpg",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        },
+        {
+            productId: 6,
+            name: "Old Navy",
+            city: "Boston",
+            url: "www.oldnavy.com",
+            image: "http://res.cloudinary.com/goodsearch/image/upload/v1439940283/hi_resolution_merchant_logos/old-navy_coupons.jpg",
+            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+        }
+
+    ]
+}
 
 module.exports = class SkypeBot {
 
@@ -106,7 +166,42 @@ module.exports = class SkypeBot {
 
                             if(action == 'getProductsByLocation' && !actionIncomplete){
 
-                                bot.reply("some custom content", true);
+
+                                var city = response.result.parameters['geo-city-us'];
+                                var productType = response.result.parameters['product'];
+
+                                var products = [];
+
+                                if(db[productType]){
+
+                                    _.each(db[productType], function(product){
+                                        if(product.city == city){
+                                            //collect
+                                            products.push(product);
+                                        }
+                                    });
+
+                                    if(products){
+                                        var customText = '';
+                                        _.each(products, function(product, index){
+
+                                            customText += index.toString() + '.' +product.name +"\n";
+
+                                        });
+
+                                        bot.reply(customText, true)
+
+
+                                    }else{
+                                        bot.reply('Couldnt find any results', true)
+                                    }
+
+
+
+
+                                }else{
+                                    bot.reply('Couldnt find any results', true)
+
 
 
                             }
